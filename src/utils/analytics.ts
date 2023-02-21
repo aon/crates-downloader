784a3b5976d39da64757fb443817965ea97a2d4b
aon <21188659+aon@users.noreply.github.com>
2023-02-21 12:00:38 -0300

    Remove GA and add nullitics

---
+++ src/utils/analytics.ts
@@ -0,0 +1,10 @@
+export const trackPersonalGithub = () => trackEvent("github_personal_opened");
+export const trackRepoGithub = () => trackEvent("github_repo_opened");
+
+export const trackEvent = (event: string) => {
+  fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}:${event}`, {
+    mode: "no-cors",
+  });
+};
+
+export const getCurrentUri = () => encodeURI(location.href);
