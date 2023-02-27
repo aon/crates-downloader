98fff7f06ede39e5553e67d0434838796a155b2f
aon <21188659+aon@users.noreply.github.com>
2023-02-27 01:35:24 -0300

    Fix analytics events path

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -3,7 +3,7 @@ export const trackRepoGithub = () => trackEvent("github_repo_opened");
 export const trackDownload = () => trackEvent("crate_downloaded");
 
 export const trackEvent = (event: string) => {
-  fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}:${event}`, {
+  fetch(`https://nullitics.com/n.gif?u=${getCurrentUri()}/:${event}`, {
     mode: "no-cors",
   });
 };
